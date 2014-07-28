a56a37507bb0620d2ff6e4284b2e0a258b5fa36f
John Safrit <jsafrit@users.noreply.github.com>
2014-07-28 13:30:16 -0400

    Added message length check and cleanup

--- socat_test.py
+++ socat_test.py
@@ -18,22 +18,26 @@ c2 = serial.Serial('COM9')
 
 assert c1.isOpen()
 assert c2.isOpen()
-os.system('ls -alF .')
+#os.system('ls -alF .')
 
-msg = 'HELLO'
+msg = 'MySecretMessage'
 print 'Sending: "%s"' % msg
 c1.write(msg)
 
+time.sleep(.1)
+
 print 'Checking comm...',
-response = c2.read(5)
+bytesWaiting = c2.inWaiting()
+print '%d bytes waiting...' % bytesWaiting,
+response = c2.read(bytesWaiting)
 print 'Got: "%s"' % response
 
 assert msg==response
 
 
 
-
+print 'Closing socat...'
 pComms.terminate()
 time.sleep(2)
-print pComms.poll()
+
 
