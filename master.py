72c3634955570f7f8dbd762703ae3fde33e76924
John Safrit <jsafrit@users.noreply.github.com>
2014-07-28 13:29:13 -0400

    Initial commit of master child.

---
+++ master.py
@@ -0,0 +1,23 @@
+import serial
+import sys
+
+assert len(sys.argv) == 2
+print sys.argv
+
+# try to open the comm
+mcomm = None
+try:
+    mcomm=serial.Serial( sys.argv[1],timeout=1)
+except serial.serialutil.SerialException:
+    print 'BOOM! @ %s' % sys.argv[1]
+    #raise serial.serialutil.SerialException
+if not mcomm or not mcomm.isOpen():
+    print "Problem opening port. Exiting..."
+    sys.exit(3)
+
+
+print mcomm
+
+
+
+mComm.close()
